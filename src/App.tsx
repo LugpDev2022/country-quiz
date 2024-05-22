202f0122c4fe5017906b19c713589188536fc489
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-21 21:15:19 -0600

    Style the question

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import QuestionBtn from './components/QuestionBtn';
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const question = 'Which country is Kuala Lumpur the capital';
 
 const App = () => {
   return (
@@ -9,11 +10,13 @@ const App = () => {
         Country Quiz
       </h1>
 
-      <nav className='flex flex-wrap gap-3 justify-center'>
+      <nav className='flex flex-wrap gap-3 justify-center mb-10'>
         {numbers.map((number) => (
           <QuestionBtn questionNumber={number} />
         ))}
       </nav>
+
+      <h2 className='text-lg font-semibold'>{question}</h2>
     </main>
   );
 };
