24e48f0e18a59b96369c944d3349db9e9ada4ac7
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-21 19:12:14 -0600

    Add the BeVietnamPro font and change to white text color

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ const App = () => {
   return (
     <main className='main'>
       <h1>Country Quiz</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+        facere?
+      </p>
     </main>
   );
 };
