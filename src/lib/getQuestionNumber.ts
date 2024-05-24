66ec14dafdb3892f7bdca5b49096e552a2504304
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-24 17:14:47 -0600

    Add doc comments to getQuestionNumber.ts

--- src/lib/getQuestionNumber.ts
+++ src/lib/getQuestionNumber.ts
@@ -2,8 +2,14 @@ import queryString from 'query-string';
 
 type ParsedQueryParams = Record<string, string | undefined>;
 
-// This function receives a query string and returns the value of the "question" parameter
-// If not value found it returns 1 by default
+/**
+ * Retrieves the value of the "question" parameter from a query string.
+ * If the "question" parameter is not found or is invalid, it returns 1 by default.
+ *
+ * @param query - The query string to parse (e.g., '?question=2').
+ * @returns The value of the "question" parameter as a number. Returns 1 if the parameter is not found or cannot be parsed to a number.
+ */
+
 export const getQuestionNumber = (query: string): number => {
   const { question } = queryString.parse(query) as ParsedQueryParams;
 
