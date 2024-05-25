4fcf740aeb8981decb643d1f40a43af4cc4abc5a
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-24 18:27:40 -0600

    Add doc comments to the app reducer

--- src/context/appReducer.ts
+++ src/context/appReducer.ts
@@ -1,11 +1,19 @@
 import type { AppState } from '../types';
 import type { Action } from './reducerTypes';
 
+/**
+ * Reducer function for managing the application state.
+ * Handles actions related to setting answers, changing the current question number, and resetting the state.
+ *
+ * @param state - The current state of the application.
+ * @param action - The action to be processed. It includes the type of action and the payload containing the necessary data.
+ * @returns The new state of the application after applying the given action.
+ */
+
 export const appReducer = (state: AppState, action: Action): AppState => {
   switch (action.type) {
-    case 'SET_ANSWER':
+    case 'SET_ANSWER': {
       // Map over the questionsData array to create a new array with updated data
-      // eslint-disable-next-line no-case-declarations
       const newQuestionsData = state.questionsData.map((question, index) =>
         // If the current index matches the questionIndex, update the selectedAnswer
         index === action.payload.questionIndex
@@ -18,6 +26,7 @@ export const appReducer = (state: AppState, action: Action): AppState => {
         questionsData: newQuestionsData,
         completedQuestions: state.completedQuestions + 1,
       };
+    }
 
     case 'SET_CURRENT_QUESTION_NUMBER':
       return { ...state, currentQuestionNumber: action.payload as number };
