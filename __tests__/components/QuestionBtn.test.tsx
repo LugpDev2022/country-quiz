763262abef469e42ab3523668796c1b590573073
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-25 13:22:42 -0600

    Tests on <QuestionBtn />

---
+++ __tests__/components/QuestionBtn.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import QuestionBtn from '../../src/components/QuestionBtn';
+import { MemoryRouter } from 'react-router';
+
+describe('Tests on <QuestionBtn />', () => {
+  test('should render the correct question number', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <QuestionBtn questionNumber={2} />
+      </MemoryRouter>
+    );
+
+    expect(getByText('2')).toBeTruthy();
+  });
+
+  test('should have the correct link', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <QuestionBtn questionNumber={2} />
+      </MemoryRouter>
+    );
+
+    const link = getByRole('link') as HTMLAnchorElement;
+    expect(link.href).toBe('http://localhost:3000/?question=2');
+  });
+
+  test('should have the correct css classes when not active', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <QuestionBtn questionNumber={2} />
+      </MemoryRouter>
+    );
+
+    const link = getByRole('link') as HTMLAnchorElement;
+    expect(link.className).toBe('question-btn gradient');
+  });
+
+  test('should have the correct css classes when active', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <QuestionBtn questionNumber={2} active />
+      </MemoryRouter>
+    );
+
+    const link = getByRole('link') as HTMLAnchorElement;
+    expect(link.className).toBe('question-btn-active');
+  });
+});
