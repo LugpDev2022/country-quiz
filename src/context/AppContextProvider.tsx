c46b92cf61d6cc0083b5093ce0e2a8843d4f5e20
LugpDev2022 <lugpdev2022@gmail.com>
2024-05-25 09:08:56 -0600

    Create useContextProvider hook

--- src/context/AppContextProvider.tsx
+++ src/context/AppContextProvider.tsx
@@ -1,47 +1,12 @@
-import { useEffect, useReducer } from 'react';
-import { useLocation, useNavigate } from 'react-router';
-
 import { AppContext } from './AppContext';
-import { questions } from '../questions';
-import { appReducer } from './appReducer';
-import { getQuestionNumber } from '../lib/getQuestionNumber';
+import { useContextProvider } from './hooks/useContextProvider';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const AppContextProvider: React.FC<Props> = ({ children }) => {
-  const { search } = useLocation();
-  const navigate = useNavigate();
-  const [state, dispatch] = useReducer(appReducer, {}, () => {
-    const currentQuestionNumber = getQuestionNumber(search);
-
-    return {
-      currentQuestionNumber,
-      questionsData: questions,
-      completedQuestions: 0,
-    };
-  });
-
-  useEffect(() => {
-    const questionNumber = getQuestionNumber(search);
-
-    dispatch({ type: 'SET_CURRENT_QUESTION_NUMBER', payload: questionNumber });
-  }, [search]);
-
-  useEffect(() => {
-    if (state.completedQuestions !== state.questionsData.length) return;
-
-    navigate('/results');
-  }, [state.completedQuestions]);
-
-  const sendAnswer = (questionIndex: number, answerNumber: 1 | 2 | 3 | 4) => {
-    dispatch({ type: 'SET_ANSWER', payload: { questionIndex, answerNumber } });
-  };
-
-  const handlePlayAgain = () => {
-    dispatch({ type: 'RESET_STATE', payload: questions });
-  };
+  const { state, handlePlayAgain, sendAnswer } = useContextProvider();
 
   return (
     <AppContext.Provider value={{ ...state, sendAnswer, handlePlayAgain }}>
---
+++ src/context/hooks/useContextProvider.ts
@@ -0,0 +1,46 @@
+import { useEffect, useReducer } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+
+import { appReducer } from '../appReducer';
+import { getQuestionNumber } from '../../lib/getQuestionNumber';
+import { questions } from '../../questions';
+
+const initializer = (search: string) => {
+  const currentQuestionNumber = getQuestionNumber(search);
+
+  return {
+    currentQuestionNumber,
+    questionsData: questions,
+    completedQuestions: 0,
+  };
+};
+
+export const useContextProvider = () => {
+  const { search } = useLocation();
+  const navigate = useNavigate();
+  const [state, dispatch] = useReducer(appReducer, search, initializer);
+
+  const { completedQuestions, questionsData } = state;
+
+  useEffect(() => {
+    const questionNumber = getQuestionNumber(search);
+
+    dispatch({ type: 'SET_CURRENT_QUESTION_NUMBER', payload: questionNumber });
+  }, [search]);
+
+  useEffect(() => {
+    if (completedQuestions === questionsData.length) {
+      navigate('/results');
+    }
+  }, [completedQuestions, questionsData.length, navigate]);
+
+  const sendAnswer = (questionIndex: number, answerNumber: 1 | 2 | 3 | 4) => {
+    dispatch({ type: 'SET_ANSWER', payload: { questionIndex, answerNumber } });
+  };
+
+  const handlePlayAgain = () => {
+    dispatch({ type: 'RESET_STATE', payload: questions });
+  };
+
+  return { state, sendAnswer, handlePlayAgain };
+};
